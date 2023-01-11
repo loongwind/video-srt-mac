bc01b2e07ba213ce46bf3844afac7a939e108531
loongwind <loongwind@gmail.com>
2023-01-11 09:13:20 +0800

    optimize get work dir

--- lib/utils/path_utils.dart
+++ lib/utils/path_utils.dart
@@ -8,13 +8,20 @@ import 'package:path_provider/path_provider.dart';
 import '../constanst.dart';
 
 class PathUtils{
+
+  static String? workDirPath;
+
   static Future<String> getWorkDirPath() async{
+    if(workDirPath != null){
+      return workDirPath!;
+    }
     Directory tempDir = await getLibraryDirectory();
     var workDir = "${tempDir.path}/$WORK_DIR_NAME";
     var dir = Directory(workDir);
     if(! (await dir.exists())){
       await dir.create();
     }
+    workDirPath = workDir;
     return workDir;
   }
 
